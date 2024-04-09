9ac6a76b0bbc936df7ee0a661dc1ba3ba9b52c32
quinnasher <pauldivineagionoja@yahoo.com>
2024-04-09 22:16:07 +0100

    added get all controller

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -3,22 +3,43 @@ import catchAsync from "../utils/catch.async.js";
 
 export const createService = catchAsync(async (req, res, next) => {
   const newService = await Service.create(req.body);
+
   res.status(201).json({
     status: "success",
     data: { newService },
   });
 });
 
-export const getService = catchAsync(async (req, res) => {
+export const getService = catchAsync(async (req, res, next) => {
   const service = await Service.findById(req.params.id);
+
   res.status(200).json({
     status: "success",
     data: { service },
   });
 });
-export const updateService = catchAsync(async (req, res) => {});
-export const deleteService = catchAsync(async (req, res) => {});
-export const getAllServices = catchAsync(async (req, res) => {});
+
+export const getAllServices = catchAsync(async (req, res, next) => {
+  const services = await Service.find({});
+
+  res.status(200).json({
+    status: "success",
+    data: { length: services.length, services },
+  });
+});
+
+export const updateService = catchAsync(async (req, res, next) => {
+  const updatedService = await Service.findByIdAndUpdate(req.params.id);
+
+  res.status(200).json({
+    status: "success",
+    data: { updatedService },
+  });
+});
+export const deleteService = catchAsync(async (req, res, next) => {
+  await Service.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
 export const getAllServicesByRating = catchAsync(async (req, res, next) => {});
 export const getAllServicesByEmergencyStatus = catchAsync(
   async (req, res, next) => {},
