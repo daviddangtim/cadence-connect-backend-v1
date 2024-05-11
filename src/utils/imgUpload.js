bf2d97b0f0a04776ea94f7ba5d4a5e39412b1842
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-05-11 12:17:37 +0100

    fixes

---
+++ src/utils/imgUpload.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+
+/* eslint-disable import/no-extraneous-dependencies */
+
+import multer from "multer";
+import path from 'node:path'
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+      cb(null,'../images')
+    },
+    filename:(req,file,cb)=>{
+      cb(null,Date.now() + path.extname(file.originalname) )
+    }
+  });
+  
+  const upload = multer({storage: storage });
+  export default upload;
