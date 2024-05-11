53b9bc2eb813d9aaecb60c8045fe165f1c99bfc7
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-05-11 14:22:15 +0100

    fix

--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 import express from "express";
 import {
   createService,
@@ -8,14 +9,13 @@ import {
   updateService,
 } from "../controllers/service.controller.js";
 import { protect, restrictTo } from "../controllers/auth.controller.js";
-import upload from "../utils/imgUpload.js";
 
 const router = express.Router();
 
 router
   .route("/")
-  .post(upload.single("coverImage"),createService)
-   .get(protect, restrictTo("admin", "client"), getAllServices);
+  .post(protect,createService)
+   .get(protect, getAllServices);
 router.route("/:id").get(getService).patch(updateService).delete(deleteService);
 
 export default router;
